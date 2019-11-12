d78eadefec5b59629b548f0a620c9b73833eadd7
Karel Buyck <57662549+karel-buyck@users.noreply.github.com>
2019-11-12 12:02:46 +0100

    commit on master

---
+++ Azure Arm/ResourceGroups/Generalcommands.ps1
@@ -0,0 +1,13 @@
+
+#List
+Get-AzResourceGroup
+
+=========================
+#Create
+
+$resourceGroup="DeleteMe"
+$location="WestEurope"
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+========================
+
